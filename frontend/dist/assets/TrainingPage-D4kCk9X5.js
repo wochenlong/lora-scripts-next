b105303cb3ef6ef8f2e8ebbc94310a98177ecd52
xixun.bu <xixun.bu@gientech.com>
2026-08-01 22:15:08 +0800

    chore(frontend): publish built Vue frontend

--- frontend/dist/assets/TrainingPage-D4kCk9X5.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./TrainingPage.vue_vue_type_script_setup_true_lang-BcBF2nQ3.js";import"./index-CmyAXMEc.js";import"./index-CQE9XeNV.js";import"./index-BTKc5RcA.js";export{o as default};
